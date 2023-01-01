d361b1b4744984b8988a3398faa12d42f25051c2
Patrick Navarro <patricknv230101@gmail.com>
2022-12-31 19:37:20 -0500

    tarjetas de instagram con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 're-usable-components';
+  userdata = [
+    {
+      name: 'Patrick',
+      location: 'New York',
+      dp: 'https://cdn-icons-png.flaticon.com/512/145/145843.png',
+      post: 'https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deserunt odit saepe vel blanditiis, recusandae quo fugiat sit iste aspernatur veritatis magnam neque similique molestias necessitatibus! Natus assumenda unde ex?'
+    },
+    {
+      name: 'Fabrizio',
+      location: 'New York',
+      dp: 'https://cdn-icons-png.flaticon.com/512/145/145843.png',
+      post: 'https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deserunt odit saepe vel blanditiis, recusandae quo fugiat sit iste aspernatur veritatis magnam neque similique molestias necessitatibus! Natus assumenda unde ex?'
+    },
+    {
+      name: 'Pablo',
+      location: 'New York',
+      dp: 'https://cdn-icons-png.flaticon.com/512/145/145843.png',
+      post: 'https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deserunt odit saepe vel blanditiis, recusandae quo fugiat sit iste aspernatur veritatis magnam neque similique molestias necessitatibus! Natus assumenda unde ex?'
+    }
+  ]
 }
